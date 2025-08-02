4847033a2fe8787c29c085ba06411c9954ebfc53
nandrepo1 <github25u7@gmail.com>
2025-08-02 09:57:37 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -76,10 +76,10 @@ GIT_TOKEN = getenv(
 
 # Only  Links formats are  accepted for this Var value.
 SUPPORT_CHANNEL = getenv(
-    "SUPPORT_CHANNEL", "https://t.me/Professor_bot_updates"
+    "SUPPORT_CHANNEL", "https://t.me/NOBITA_NETW0RK"
 )  # Example:- https://t.me/akaChampu
 SUPPORT_GROUP = getenv(
-    "SUPPORT_GROUP", "https://t.me/Professor_bot_updates"
+    "SUPPORT_GROUP", "https://t.me/NOBITA_BOTS"
 )  # Example:- https://t.me/TheChampuClub
 # Set it in True if you want to leave your assistant after a certain amount of time. [Set time via AUTO_LEAVE_ASSISTANT_TIME]
 AUTO_LEAVING_ASSISTANT = getenv("AUTO_LEAVING_ASSISTANT", True)
@@ -101,7 +101,7 @@ TELEGRAM_DOWNLOAD_EDIT_SLEEP = int(getenv("TELEGRAM_EDIT_SLEEP", 5))
 
 
 # Your Github Repo.. Will be shown on /start Command
-GITHUB_REPO = getenv("GITHUB_REPO", "https://github.com/shizukaofficial/shizukaxchampuoffoceal")
+GITHUB_REPO = getenv("GITHUB_REPO", "https://github.com/nandrepo1/Olivachampu")
 
 
 # Spotify Client.. Get it from https://developer.spotify.com/dashboard
